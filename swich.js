3c0c1cb632286572b4c310050d568aaa6a2ec27c
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-03 21:20:15 +0600

    first commit

---
+++ swich.js
@@ -0,0 +1,78 @@
+var color = 'red';
+
+if (color == blue) {
+    console.log('the color is blue');
+}
+
+
+else if (color == yellow) {
+    console.log('the color is yellow');
+}
+
+
+else if (color == green) {
+    console.log('the color is green');
+}
+
+
+else if (color == magenda) {
+    console.log('the color is magenda');
+}
+
+
+else if (color == black) {
+    console.log('the color is black');
+}
+
+
+else if (color == white) {
+    console.log('the color is white');
+}
+
+
+else if (color == red) {
+    console.log('the color is red');
+}
+
+else {
+    console.log('the color is sky');
+}
+
+
+
+
+
+switch (color) {
+
+    case 'blue':
+        console.log('the color is blue');
+        break;
+
+
+    case 'yellow':
+        console.log('the color is yellow');
+        break;
+
+    case 'green':
+        console.log('the color is green');
+        break;
+
+
+    case 'magenda':
+        console.log('the color is magenda');
+        break;
+
+
+    case 'black':
+        console.log('the color is black');
+        break;
+
+
+    case 'white':
+        console.log('the color is white');
+        break;
+
+    case 'red':
+        console.log('the color is red');
+        break
+}
